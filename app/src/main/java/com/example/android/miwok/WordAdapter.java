e28861afba7537673f57fd539707d8e8d9a01d4c
mbeevor <mbeevor@gmail.com>
2017-05-05 18:42:23 +0100

    WordAdapter (now working  - comments edited)

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -34,16 +33,16 @@ public class WordAdapter extends ArrayAdapter<Word> {
         Word Word = getItem(position);
 
         // Find the TextView in the list_item.xml layout with the ID version_name
-        TextView nameTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
         // Get the version name from the current Word object and
-        // set this text on the name TextView
-        nameTextView.setText(Word.getMiwokTranslation());
+        // set this text on the Miwok TextView
+        miwokTextView.setText(Word.getMiwokTranslation());
 
         // Find the TextView in the list_item.xml layout with the ID version_number
-        TextView numberTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
-        // Get the version number from the current AndroidFlavor object and
-        // set this text on the number TextView
-        numberTextView.setText(Word.getDefaultTranslation());
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        // Get the version number from the current Word object and
+        // set this text on the Default TextView
+        defaultTextView.setText(Word.getDefaultTranslation());
 
         // Return the whole list item layout (containing 2 TextViews)
         // so that it can be shown in the ListView
