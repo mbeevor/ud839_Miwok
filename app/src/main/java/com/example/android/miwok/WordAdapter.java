cc4f52157226c49fe435c32fb3634af133b49aa1
mbeevor <mbeevor@gmail.com>
2017-05-05 18:23:03 +0100

    WordAdapter (now working)

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -2,9 +2,12 @@ package com.example.android.miwok;
 
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -20,6 +23,30 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link Word} object located at this position in the list
+        Word Word = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID version_name
+        TextView nameTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        // Get the version name from the current Word object and
+        // set this text on the name TextView
+        nameTextView.setText(Word.getMiwokTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID version_number
+        TextView numberTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        // Get the version number from the current AndroidFlavor object and
+        // set this text on the number TextView
+        numberTextView.setText(Word.getDefaultTranslation());
+
+        // Return the whole list item layout (containing 2 TextViews)
+        // so that it can be shown in the ListView
+        return listItemView;
     }
 }
