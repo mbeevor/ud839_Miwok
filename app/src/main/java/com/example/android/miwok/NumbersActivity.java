7a72f10d5b6cfed5303a9fd553d30c37bd279d3b
mbeevor <mbeevor@gmail.com>
2017-05-21 14:49:06 +0100

    drawable

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,12 +1,13 @@
 package com.example.android.miwok;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.media.AudioManager;
 
 import java.util.ArrayList;
 
@@ -15,13 +16,15 @@ public class NumbersActivity extends AppCompatActivity {
     private MediaPlayer mediaPlayer;
     private AudioManager audioManager;
 
-    AudioManager.OnAudioFocusChangeListener focusChangeListener =
+    AudioManager.OnAudioFocusChangeListener audioFocusChangeListener =
             new AudioManager.OnAudioFocusChangeListener() {
+
+                @Override
                 public void onAudioFocusChange(int focusChange) {
                     if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT) {
                         // Pause playback because your Audio Focus was
                         // temporarily stolen, but will be back soon.
-                        // i.e. for a phone call
+                        // e.g. for a phone call
                         mediaPlayer.pause();
                     } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
                         // Stop playback, because you lost the Audio Focus.
@@ -29,20 +32,19 @@ public class NumbersActivity extends AppCompatActivity {
                         // Remember to unregister your controls/buttons here.
                         // And release the kra — Audio Focus!
                         // You’re done.
-                        mediaPlayer.stop();
                         releaseMediaPlayer();
                     } else if (focusChange ==
                             AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
                         // Lower the volume, because something else is also
                         // playing audio over you.
-                        // i.e. for notifications or navigation directions
+                        // e.g. for notifications or navigation directions
                         // Depending on your audio playback, you may prefer to
                         // pause playback here instead. You do you.
                         mediaPlayer.pause();
                     } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
                         // Resume playback, because you hold the Audio Focus
                         // again!
-                        // i.e. the phone call ended or the nav directions
+                        // e.g. the phone call ended or the nav directions
                         // are finished
                         // If you implement ducking and lower the volume, be
                         // sure to return it to normal here, as well.
@@ -55,7 +57,7 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
-        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+
         final ArrayList<Word> words = new ArrayList<Word>();
         words.add(new Word("one", "lutti", R.drawable.number_one, R.raw.number_one));
 
@@ -91,20 +93,23 @@ public class NumbersActivity extends AppCompatActivity {
                 releaseMediaPlayer();
                 Word word = words.get(position);
 
-                //Check if audiofocus is granted, and if yes, start mediaPlayer
-                int result = audioManager.requestAudioFocus(focusChangeListener,
-                        AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                // Initiate AudioManager
+                audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
-                if(result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                // Request AudioFocus
+                int result = audioManager.requestAudioFocus(audioFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                // Request for AudioFocus is successful
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
                     mediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getAudioResourceId());
                     mediaPlayer.start();
                     mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                        @Override
-                        public void onCompletion(MediaPlayer mediaPlayer) {
-                            releaseMediaPlayer();
-                        }
-                    });
-                }
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        releaseMediaPlayer();
+                    }
+                });
+            }
             }
         });
 
